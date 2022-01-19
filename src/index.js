f91468b1e44b037848841b4d69e25b6f872ce2f2
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-01-19 18:23:00 +0530

    feat : basic Card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import "./styles.css";
 
-const chipInput = document.querySelector("#chip-input");
+const chipInput = document.querySelector(".chip-input");
 
 function addChip(text) {
   const chipContainer = document.querySelector("#chip-box");
   let chip = document.createElement("div");
-  chip.classList.add("chip");
-
+  chip.className="chip flex-center";
   chip.innerHTML = `<p>${text}</p>
   <button onclick="del(event)" class="x-btn" >x</button>`;
   chipContainer.appendChild(chip);
@@ -14,12 +12,15 @@ function addChip(text) {
 chipInput.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     if (event.target.value === "") alert("enter chip text");
-    addChip(event.target.value);
-    event.target.value = "";
+    else {
+      addChip(event.target.value);
+      event.target.value = "";
+    }
   }
 });
 window.del = function del(event) {
   console.log(event.target.parentElement);
   event.target.parentElement.remove();
 };
-window.onload = () => addChip("Lemon");
+
+ addChip("Lemon");
